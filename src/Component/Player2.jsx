1086199ea50dce839811665a45f664b5281c93b8
Kuro-sam1210 <samuelmajasan6@gmail.com>
2025-08-08 11:06:53 +0100

    mussic

--- src/Component/Player2.jsx
+++ src/Component/Player2.jsx
@@ -3,6 +3,9 @@ import { MdNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
 import { CiPause1 } from "react-icons/ci";
 import { CiHome } from "react-icons/ci"
+import { IoMusicalNotesOutline } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
 
 export default function MusicPlayer() {
   return (
@@ -69,19 +72,10 @@ export default function MusicPlayer() {
       </div>
 
       <div className="bottom-nav">
-        <svg className="nav-icon inactive" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-        </svg>
-        <svg className="nav-icon active" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
-        </svg>
-        <svg className="nav-icon inactive" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-        </svg>
-        <svg className="nav-icon inactive" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
+        <CiHome className="nav-icon inactive" />
+        <IoMusicalNotesOutline className="nav-icon active" />
+        <CiHeart className="nav-icon inactive" />
+        <CiLocationOn className="nav-icon inactive" />
       </div>
     </div>
   );
